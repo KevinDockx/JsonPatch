c716cc6428742af544c8562bdec6cee809487082
KevinDockx <kevin.dockx@gmail.com>
2014-11-04 17:40:16 -0800

    Assembly info

--- src/Marvin.JsonPatch/Properties/AssemblyInfo.cs
+++ src/Marvin.JsonPatch/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Marvin.HttpPatch")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Json Patch (RFC 6902) support for .NET")]
+[assembly: AssemblyDescription("JSON Patch (RFC 6902) support for .NET to easily allow & apply partial REST-ful service (through Web API) updates from any client (portable class library).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Marvin.HttpPatch")]
+[assembly: AssemblyCompany("Kevin Dockx (Marvin)")]
+[assembly: AssemblyProduct("Json Patch (RFC 6902) support for .NET")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
