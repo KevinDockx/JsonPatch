00c1070acb5ad6ca05b141fb0929f876d7d51536
KevinDockx <kevin.dockx@gmail.com>
2015-02-24 19:33:51 +0100

    Migrate to xUnit

---
+++ src/Marvin.JsonPatch.XUnitTest/SimpleDTOWithNestedDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marvin.JsonPatch.XUnitTest
+{
+    public class SimpleDTOWithNestedDTO
+    {
+        public int IntegerValue { get; set; }
+
+        public NestedDTO NestedDTO { get; set; }
+
+        public SimpleDTO SimpleDTO { get; set; }
+
+        public SimpleDTOWithNestedDTO()
+        {
+            this.NestedDTO = new NestedDTO();
+            this.SimpleDTO = new SimpleDTO();
+        }
+    }
+}
