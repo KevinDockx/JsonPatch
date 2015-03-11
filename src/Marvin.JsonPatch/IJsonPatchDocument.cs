353bcdf6a385580778ab576df213302fdb608045
KevinDockx <kevin.dockx@gmail.com>
2015-03-11 17:29:59 +0100

    IJsonPatchDocument should be internal, not public

--- src/Marvin.JsonPatch/IJsonPatchDocument.cs
+++ src/Marvin.JsonPatch/IJsonPatchDocument.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Marvin.JsonPatch
 {
-    public interface IJsonPatchDocument
+    internal interface IJsonPatchDocument
     {
         List<Operation> GetOperations();
     }
