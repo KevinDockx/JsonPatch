ed61f1133db9b224132e68bf1527f553fadddc72
Kevin Djordjevic <Kevin.Djordjevic@amtrustgroup.com>
2016-04-26 11:21:25 -0400

    Added unit test coverage.

---
+++ src/Marvin.JsonPatch.XUnitTest/ObjectAdapterTestsOnDerived.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Xunit;
+
+namespace Marvin.JsonPatch.XUnitTest
+{
+    public class ObjectAdapterTestsOnDerived
+    {
+        [Fact]
+        public void ReplacePropertyWithNewKeyword()
+        {
+            var doc = new DerivedDTO();
+            var replacementCollection = new ReadOnlyCollection<int>(new List<int>() { 1, 2, 3, 4 });
+            JsonPatchDocument<DerivedDTO> patchDoc = new JsonPatchDocument<DerivedDTO>();
+            patchDoc.Replace(o => o.IntegerList, replacementCollection);
+            patchDoc.ApplyTo(doc);
+            Assert.Equal(replacementCollection, doc.IntegerList);
+        }
+    }
+}
